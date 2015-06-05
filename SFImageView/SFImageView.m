9e6f98dfa0b6b5555ade56ead35f388b5d534a0e
truthbeauty <303040805@qq.com>
2015-06-05 22:21:12 +0800

    使用__weak标识CGImage

--- SFImageView/SFImageView.m
+++ SFImageView/SFImageView.m
@@ -7,7 +7,6 @@
 //
 
 #import "SFImageView.h"
-//#define UNFINISHED 1
 
 @implementation SFImageView
 
@@ -16,32 +15,28 @@
     
     if(self == [super initWithFrame:frame]){
         
-        NSUInteger frameCount = 7;
+        //总帧数
+        NSUInteger FRAME_COUNT = 7;
 
-        //设置动画
-        NSMutableArray* images = [[NSMutableArray alloc] initWithCapacity:frameCount];
-        for (int i = 0; i < frameCount; i++){
-            UIImage* image = [UIImage imageNamed:[NSString stringWithFormat:@"%d",(i + 1)]];
-#ifdef UNFINISHED
-            //在我的项目里,多次调用 SFImageView 在加载图片时会提示访问的非法指针。
-            [images addObject:(__bridge id)(image.CGImage)];
-#else
-            CGImageRef cgImage = CGImageCreateCopy(image.CGImage);
-            [images addObject:(__bridge id)(cgImage)];
-            CGImageRelease(cgImage);
-#endif
+        //设置每帧动画图片
+        NSMutableArray* images = [[NSMutableArray alloc] initWithCapacity:FRAME_COUNT];
+        UIImage* image;
+        for (int i = 0; i < FRAME_COUNT; i++){
+            image = [UIImage imageNamed:[NSString stringWithFormat:@"%d",(i + 1)]];
+            [images addObject:(__weak id)(image.CGImage)];
         }
         
-        //设置帧动画发生时间,
-        NSArray* keyTimes = [[NSArray alloc] initWithObjects:@(0.0f),@(1.0f/7.0f),@(2.0f/7.0f),@(3.0f/7.0f),@(4.0f/7.0f),@(5.0f/7.0f),@(6.0f/7.0f),@(1), nil];
+        //每帧动画发生在总时长durationsh上的位置,
+        //第一帧时间为0,参见 kCAAnimationDiscrete 属性介绍.
+        NSArray* keyTimes = [[NSArray alloc] initWithObjects:@0,@(1.0f/7.0f),@(2.0f/7.0f),@(3.0f/7.0f),@(4.0f/7.0f),@(5.0f/7.0f),@(6.0f/7.0f),@1, nil];
         
         CAKeyframeAnimation *animation = [CAKeyframeAnimation animationWithKeyPath:@"contents"];
         animation.calculationMode = kCAAnimationDiscrete;
         animation.keyTimes = keyTimes;
         animation.values = images;
-        animation.duration = 1;
+        animation.duration = 7;     //总时长
         animation.repeatCount = HUGE_VAL;
-        [self.layer addAnimation:animation forKey:@"customAnimation"];
+        [self.layer addAnimation:animation forKey:nil];
         
     }
     
